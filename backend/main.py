457599a5d3cb4bead2b1b1b59d373bcfca795806
Poonam Navale <poonamnavale94@gmail.com>
2025-10-06 23:55:08 -0400

    sync latest local changes

--- backend/main.py
+++ backend/main.py
@@ -41,9 +41,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:3002"
+        "https://aipoweredcompetativeanalysis.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
